96347180a650c60a5841b75dfbcfb106258f0bc1
shadySource <shadysource2@gmail.com>
2018-05-21 22:57:27 -0400

    add hello-world

---
+++ macros/ls/ls.rs
@@ -0,0 +1,77 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::ffi::OsString;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    parse(&args);
+    Ok(())
+}
+
+fn parse(args: &[String]){
+    let args: &[String] = &args[1..args.len()]; // ignore .exe path
+    check_help(args);
+    match args.len() {
+        0 => list_dir(&Path::new(".")),
+        1 => list_dir(&Path::new(&args[0])),
+        _ => {
+            print_usage();
+            exit();
+        },
+    };
+}
+
+fn list_dir(dir: &Path){
+    if let Ok(entries) = fs::read_dir(dir) {
+        let entries = entries.collect::<Vec<_>>();
+        let length = entries.len();
+        let mut files: Vec<OsString> = Vec::with_capacity(length);
+        let mut folders: Vec<OsString> = Vec::with_capacity(length);
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if entry.path().is_dir() { // folder
+                    // Here, `entry` is a `DirEntry`.
+                    folders.push(entry.file_name());
+                } else { // file
+                    // Here, `entry` is a `DirEntry`.
+                    files.push(entry.file_name());
+                }
+            }
+        }
+        let esc_char = vec![27];
+        let esc = String::from_utf8(esc_char).unwrap();
+        let reset: u8 = 0;
+        let bright: u8 = 1;
+        let black: u8 = 30;
+        let red: u8 = 31;
+        folders.into_iter()
+               .map(|x| )
+               .collect::<Vec<_>>();
+        for folder in folders{
+            print!("{:?}\t", folder);
+        }
+        println!();
+        println!("{}", files.len());   
+    }
+}
+
+fn check_help(args: &[String]){
+    for arg in args {
+        if arg == "-h" || arg == "--help" {
+            print_usage();
+            exit();
+        }
+    }
+}
+
+fn print_usage() {
+    println!("USAGE:");
+    println!("ls <path/to/path1/> ... <path/to/pathN/>\n");
+    println!("Outputs the files and folders in path1 ... pathN");
+    println!("Outputs the files and folders of current path if no arguments given.\n\n");
+}
+
+fn exit() {
+    std::process::exit(0);
+}
