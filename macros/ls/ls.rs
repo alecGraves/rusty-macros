5f48c2425410824d3d2a20ed70c385dd70c9c947
shadySource <shadysource2@gmail.com>
2018-05-22 10:36:09 -0400

    improve speed and printing of filenames

--- macros/ls/ls.rs
+++ macros/ls/ls.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::fs;
 use std::path::Path;
-use std::ffi::OsString;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -24,32 +23,39 @@ fn parse(args: &[String]){
 
 fn list_dir(dir: &Path){
     if let Ok(entries) = fs::read_dir(dir) {
+
         let entries = entries.collect::<Vec<_>>();
         let length = entries.len();
-        let mut files: Vec<OsString> = Vec::with_capacity(length);
-        let mut folders: Vec<OsString> = Vec::with_capacity(length);
+        let mut files: Vec<String> = Vec::with_capacity(length);
+        let mut folders: Vec<String> = Vec::with_capacity(length);
+
         for entry in entries {
             if let Ok(entry) = entry {
+                let name_string = match entry.file_name().into_string() {
+                        Ok(x) => x,
+                        Err(_x) => panic!("error getting file name")
+                    };
+
                 if entry.path().is_dir() { // folder
                     // Here, `entry` is a `DirEntry`.
-                    folders.push(entry.file_name());
+                    folders.push(name_string);
                 } else { // file
                     // Here, `entry` is a `DirEntry`.
-                    files.push(entry.file_name());
+                    files.push(name_string);
                 }
             }
         }
-        println!("---Folders---");
-        for folder in folders{
-            print!("{:?}\t", folder);
+        let mut print_string = String::from("---Folders---\n");
+        for folder in folders {
+            print_string += &folder;
+            print_string += "\n";
         }
-        println!();
-        println!();
-        println!("---Files---");
+        print_string += "\n\n---Files---\n";
         for file in files{
-            print!("{:?}\t", file);
+            print_string += &file;
+            print_string += "\n";
         }
-        println!();
+        print!("{}", print_string);
     }
 }
 
